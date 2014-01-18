5a1a68db0ad6848fdd8a0af0c0bcac8b90bca59e
czengg <czeng.andrew.cmu.edu>
2014-01-18 04:21:46 -0500

    added login, signup, and launch pages with proper url routes HBIC

--- hirepages/hirepages/urls.py
+++ hirepages/hirepages/urls.py
@@ -7,6 +7,8 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'hirepages.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+    url(r'^signup/$', views.signup),
+    url(r'^login/$', views.login),
 
     url(r'^admin/', include(admin.site.urls)),
 )
