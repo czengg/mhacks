ac63a998e4e5077d56ed6d79a1b4e5961e966060
czengg <czeng.andrew.cmu.edu>
2014-01-18 07:27:05 -0500

    signup.html inject python BULLSHIT

--- hirepages/hirepagesApp/forms.py
+++ hirepages/hirepagesApp/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.forms import extras
 
 class SignupForm(forms.Form):
     email = forms.CharField(max_length=30)    
@@ -6,11 +7,12 @@ class SignupForm(forms.Form):
     firstName = forms.CharField(max_length=30)
     middleName = forms.CharField(max_length=30)
     lastName = forms.CharField(max_length=30)
-    dateOfBirth = forms.DateField()
+    dateOfBirth = forms.DateField(widget=extras.SelectDateWidget)
     workNumber = forms.CharField(max_length=12)
     cellNumber = forms.CharField(max_length=12)
 
     RECRUITING = 0
     LOOKING = 1
     ROLE_IN_SYSTEM = ( (LOOKING, 'Looking'), (RECRUITING, 'Recruiting'),)
-    role = forms.IntegerField()
+    role = forms.MultipleChoiceField(widget=forms.RadioSelect,
+                                        choices=ROLE_IN_SYSTEM)
