6f57c8778101e0a93cbeb8207ca686e65234fd34
czengg <czeng.andrew.cmu.edu>
2014-01-18 11:55:49 -0500

    more edits in views BULLSHITTT

--- hirepages/hirepages/settings.py
+++ hirepages/hirepages/settings.py
@@ -43,12 +43,12 @@ INSTALLED_APPS = (
 )
 
 MIDDLEWARE_CLASSES = (
-    #'django.contrib.sessions.middleware.SessionMiddleware',
-    #'django.middleware.common.CommonMiddleware',
-    #'django.middleware.csrf.CsrfViewMiddleware',
-    #'django.contrib.auth.middleware.AuthenticationMiddleware',
-    #'django.contrib.messages.middleware.MessageMiddleware',
-    #'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
 ROOT_URLCONF = 'hirepages.urls'
--- hirepages/hirepages/urls.py
+++ hirepages/hirepages/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-from hirepagesApp.views import signup, login
+from hirepagesApp.views import signup, login, create_looking_page
 from django.views.generic import TemplateView
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
@@ -14,6 +14,7 @@ urlpatterns = patterns('',
     url(r'^$', TemplateView.as_view(template_name="index.html")),
     url(r'^signup', signup),
     url(r'^login', login),
+    url(r'^createlooking', create_looking_page)
 
     #url(r'^admin/', include(admin.site.urls)),
 ) 
